54685136ab25fdfb8d21a9f0739fe8ff59b7a830
Shubham Shrivastava <65484651+Shubham242000@users.noreply.github.com>
2021-03-13 10:53:56 +0530

    Update age.js

--- age.js
+++ age.js
@@ -5,14 +5,25 @@ document.getElementById('data').onclick = abc;
 function abc(){
     var d = new Date;
     var current_year = d.getFullYear();
-    var birth = x.value;
+    var birth =  (x.value);
+    console.log(birth);
+    console.log(typeof(birth))
+   
+    var check = isNaN(birth);
     var days = (current_year-1-birth) * 365;
-    
+  if(!check)
+   { 
     var p = '';
     p  = '<h1> You are more than ' + days + ' Days Old!';
-
     document.getElementById('block').innerHTML = p;
-
+    }
+    else
+    {
+        var p = '';
+        p = '<h1>Invalid Year</h1>';
+        document.getElementById('block').innerHTML = p;
+    }    
+    
 }
 
 // function getdays(x.input.data){
